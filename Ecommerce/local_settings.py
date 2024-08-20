1935c9a87e61c2a003b7ee1729019b69a0809455
ViniciusLima99 <vinicius.lima9910@gmail.com>
2024-08-20 13:55:39 -0300

    commit

--- Ecommerce/local_settings.py
+++ Ecommerce/local_settings.py
@@ -2,7 +2,7 @@ SECRET_KEY = ''
 
 # DEBUG= True
 
-# ALLOWED_HOSTS="127.0.0.1, localhost, 51.44.19.194"
+# ALLOWED_HOSTS="127.0.0.1, localhost"
 
 DB_ENGINE="django.db.backends.postgresql"
 POSTGRES_DB=""
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Projeto_E-Commerce
 Aplicação Web de um E-Commerce feita com Django.
+
+local_settings.py precisa ser preenchido com as informações da Database
